12bd597b0193ec7ed5c1618054f74e6c2c084638
Mattie <matthew.hall@serenetangerine.com>
2020-09-16 22:01:38 -0600

    added interface as an argument

--- mass-wake-on-lan.py
+++ mass-wake-on-lan.py
@@ -7,39 +7,45 @@ import argparse
 
 def getArguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('file', help='json file that contains target mac addresses', type=str)
+    parser.add_argument('-i', help='interface from which to send wake on lan packets', type=str)
+    parser.add_argument('-file', help='json file that contains target mac addresses', type=str)
 
     args = parser.parse_args()
     return args
 
 
-def wakeMac(mac):
-    #subprocess.call(['etherwake', '-i eth0 %s' % mac]):
-    print('Would run etherwake -i eth0 %s' % mac)
+def wakeMac(mac, interface):
+    #subprocess.Popen(['etherwake', '-i', '%s' % interface, '%s' % mac]):
+    print('Would run etherwake -i %s %s' % (interface, mac))
     return
 
 
-def wakeGroup(group):
+def pingTarget(mac, ip, interface):
+    print('\nPinging %s...' % (ip))
+    child = subprocess.Popen(['ping', '-c', '1', '%s' % (ip)], stdout=subprocess.PIPE)
+    streamdata = child.communicate()[0]
+    # check exit code
+    if child.returncode != 0:
+        wakeMac(mac, interface)
+    else:
+        print('%s is up!' % (ip))
+
+
+
+def wakeGroup(group, interface):
     for target in group:
         # this part will be multi threaded
         mac = target['mac']
         ip = target['ip']
-        # ping target
-        print('\nPinging %s...' % (ip))
-        child = subprocess.Popen(['ping', '-c', '1', '%s' % (ip)], stdout=subprocess.PIPE)
-        streamdata = child.communicate()[0]
-        # check exit code
-        if child.returncode != 0:
-            wakeMac(mac)
-        else:
-            print('%s is up!' % (ip))
-    return
+        pingTarget(mac, ip, interface)
+        return
 
 
 
 def main():
     args = getArguments()
     file = args.file
+    interface = args.i
 
     # load json
     # will need validate that the json is structured properly before progressing
@@ -48,7 +54,7 @@ def main():
     
     # cycle through the groups and send wake command to macs in that group
     for group in data['groups']:
-        wakeGroup(group)
+        wakeGroup(group, interface)
             
 
 
